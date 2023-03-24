52ec2759f80ad788bdc80ef86049137d491171b3
patilpratap97 <patilpratap2101@gmail.com>
2023-03-24 11:48:56 +0530

    updated

--- application/controllers/user/Login.php
+++ application/controllers/user/Login.php
@@ -14,7 +14,7 @@ $this->load->model('User_Login_Model');
 $validate=$this->User_Login_Model->validatelogin($emailid,$password,$status);
 if($validate)
 {
-$this->session->set_userdata('uid',$validate->$id);
+$this->session->set_userdata('uid',$validate);
 setcookie('uid',$validate->uid,time() + (1*60*60));
 return redirect('user/dashboard');
 } else{
